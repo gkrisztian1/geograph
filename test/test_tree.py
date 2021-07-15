70206cbe67f520ccc9f07beca60d268d02ed8d8f
gkrisztian <g.krisztian@outlook.com>
2021-07-15 06:35:07 +0200

    Tree is rolled back to a naive implementation

--- test/test_tree.py
+++ test/test_tree.py
@@ -1,14 +1,14 @@
-from nodegraph import binarySearchTree
+from nodegraph import Tree
 from nodegraph import Node
 from random import randint, uniform
 
 def test_filter_node_duplicates():
     noise = lambda : uniform(-1e-5, 1e-5)
-    tree = binarySearchTree()
+    tree = Tree()
 
     for i in range(500):
         X = randint(-1, 1) + noise()
         Y = randint(-1, 1) + noise()
         tree.insert(Node(X, Y))
 
-    assert len(list(tree.depthFirstSearch_POSTorder())) == 9
+    assert len(list(tree)) == 9
