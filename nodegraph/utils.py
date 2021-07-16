948be1147177c786b5195b2c1aea925e66c5a1fd
GKrisztian <g.krisztian@outlook.com>
2021-07-16 08:59:30 +0200

    time measuring decorator added

--- nodegraph/utils.py
+++ nodegraph/utils.py
@@ -1,5 +1,18 @@
 from uuid import uuid4
-
+from time import perf_counter
+from functools import wraps
 
 def getID():
     return int(uuid4())
+
+
+
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = perf_counter()
+        result = f(*args, **kw)
+        te = perf_counter()
+        print(f'function: {f.__name__} took: {te-ts:2.5f} sec.')
+        return result
+    return wrap
